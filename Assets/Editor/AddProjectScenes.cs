b4b6626763146c89d91ae54e8ca59eacad447a89
KittyMac <rocco@chimerasw.com>
2015-04-17 14:35:21 -0400

    Test PlanetUnity scene

---
+++ Assets/Editor/AddProjectScenes.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+
+[InitializeOnLoad]
+[ExecuteInEditMode]
+class AddProjectScenes : AssetPostprocessor
+{
+	static AddProjectScenes ()
+	{
+		ReloadBuildScenes ();
+	}
+
+	static void ReloadBuildScenes ()
+	{
+		//Debug.Log ("AddProjectScenes");
+
+		List<string> SceneFiles = new List<string> ();
+
+		DirectoryInfo dir = new DirectoryInfo (Application.dataPath + "/Scenes");
+		FileInfo[] info = dir.GetFiles("*.*");
+		foreach (FileInfo fileInfo in info)  {
+			if (fileInfo.FullName.EndsWith (".unity")) {
+				SceneFiles.Add (fileInfo.Name);
+			}
+		}
+
+		SceneFiles.Sort(delegate(string x, string y)
+			{
+				int indexX = 99;
+				int indexY = 99;
+
+				if(x.Contains("_")){
+					indexX = int.Parse(x.Substring(0, x.IndexOf("_")));
+				}
+
+				if(y.Contains("_")){
+					indexY = int.Parse(y.Substring(0, y.IndexOf("_")));
+				}
+
+				return indexX.CompareTo(indexY);
+			});
+
+		EditorBuildSettingsScene[] newSettings = new EditorBuildSettingsScene[SceneFiles.Count];
+
+		int index = 0;
+		foreach (string sceneFile in SceneFiles) {
+			newSettings [index] = new EditorBuildSettingsScene ("Assets/Scenes/" + sceneFile, true);
+			index++;
+		}
+
+		EditorBuildSettings.scenes = newSettings;
+	}
+
+
+	static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromPath)
+	{
+		ReloadBuildScenes ();
+	}
+}
---
+++ Assets/Editor/CopyHiddenResources.cs
@@ -0,0 +1,62 @@
+﻿/*
+using UnityEditor;
+using System.Diagnostics;
+
+public class CopyHiddenResources 
+{
+    [MenuItem("MyTools/Windows Build With Postprocess")]
+    public static void BuildGame ()
+    {
+        // Get filename.
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] {"Assets/Scene1.unity", "Assets/Scene2.unity"};
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // Copy a file from the project folder to the build folder, alongside the built game.
+        FileUtil.CopyFileOrDirectory("Assets/WebPlayerTemplates/Readme.txt", path + "Readme.txt");
+
+        // Run the game (Process class from System.Diagnostics).
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + "BuiltGame.exe";
+        proc.Start();
+    }
+}
+*/
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+// /Volumes/Development/smallplanet/domvs_Prototype/Builds/iOS_Enterprise
+using System.IO;
+using System;
+	
+public class CopyHiddenResources {
+	
+	[PostProcessBuildAttribute(1)]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+		Debug.Log (pathToBuiltProject);
+
+		Copy (pathToBuiltProject + "/../../Movies/", pathToBuiltProject + "/Data/Raw/");
+
+	}
+
+	public static void Copy(string sourceDir, string targetDir)
+	{
+		Directory.CreateDirectory (targetDir);
+
+		foreach (var file in Directory.GetFiles(sourceDir)) {
+			try{
+				FileUtil.CopyFileOrDirectory(file, Path.Combine (targetDir, Path.GetFileName (file)));
+			}catch(IOException e){
+
+			}
+		}
+
+		foreach (var directory in Directory.GetDirectories(sourceDir)) {
+			FileUtil.CopyFileOrDirectory(directory, Path.Combine (targetDir, Path.GetFileName (directory)));
+		}
+	}
+}
